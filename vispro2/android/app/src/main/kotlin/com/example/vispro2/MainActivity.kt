bffcb5ad3758301d8894d5d230d2eebb8c0fa154
shunaria <shunnoking@gmail.com>
2025-10-08 19:56:24 +0800

    checkpoint 1

---
+++ vispro2/android/app/src/main/kotlin/com/example/vispro2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.vispro2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ vispro2/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Deep Link Demo',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomeScreen(),
+        '/details': (context) => DetailScreen(),
+      },
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Home')),
+      body: Center(
+        child: Text('Welcome! Try opening myapp://details/42'),
+      ),
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Details')),
+      body: Center(child: Text('You are on the Detail screen!')),
+    );
+  }
+}
